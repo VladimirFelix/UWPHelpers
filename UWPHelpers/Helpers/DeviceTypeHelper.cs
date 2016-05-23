2ac60bc41bebaa439fd55692918bf5cac66263fa
Hugox <Hugox@DESKTOP-I7BHTVL>
2016-05-23 00:33:35 -0500

    Add project files.

---
+++ UWPHelpers/Helpers/DeviceTypeHelper.cs
@@ -0,0 +1,35 @@
+﻿using Windows.System.Profile;
+using Windows.UI.ViewManagement;
+
+namespace UWPHelpers.Helpers
+{
+    public static class DeviceFamilyHelper
+    {
+        public static DeviceFamilyType GetDeviceFamilyType()
+        {
+            switch (AnalyticsInfo.VersionInfo.DeviceFamily)
+            {
+                case "Windows.Mobile":
+                    return DeviceFamilyType.Phone;
+                case "Windows.Desktop":
+                    return UIViewSettings.GetForCurrentView().UserInteractionMode == UserInteractionMode.Mouse ? DeviceFamilyType.Desktop : DeviceFamilyType.Tablet;
+                case "Windows.Universal":
+                    return DeviceFamilyType.IoT;
+                case "Windows.Team":
+                    return DeviceFamilyType.SurfaceHub;
+                default:
+                    return DeviceFamilyType.Other;
+            }
+        }
+    }
+
+    public enum DeviceFamilyType
+    {
+        Phone,
+        Desktop,
+        Tablet,
+        IoT,
+        SurfaceHub,
+        Other
+    }
+}
